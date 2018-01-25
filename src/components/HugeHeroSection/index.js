4ff6bfaaa07f5b1df4793c4e60465c23fe239219
Dean Shelton <deanshelton913@gmail.com>
2018-01-25 15:12:33 -0800

    quit my job

--- src/components/HugeHeroSection/index.js
+++ src/components/HugeHeroSection/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './HugeHeroSection.scss';
+import mascot from './dragon.png';
 
 class HugeHeroSection extends React.Component {
   render() {
     return (
       <section className={s.container}>
         <div className={s.content}>
-          <h1>I am a header</h1>
+          <h1>Next level fanny pack ramps</h1>
           <span>Blah blah blah</span>
+          <div className={s.mascot}>
+            <img src={mascot} alt="mascot" />
+          </div>
         </div>
       </section>
     );
